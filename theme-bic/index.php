0eeb2c997d374af43939c34f824c0f8a68a1bc6e
K4milo <kmilo.vanegas@gmail.com>
2017-01-10 05:01:27 -0500

    desarrollo index

---
+++ theme-bic/index.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<div class="container">
+  <div class="row">
+		<!--banner-->
+		<?php get_template_part('inc/banner'); ?>
+      
+
+  </div><!-- /.row -->
+</div><!-- /.container -->
+
+<?php get_footer(); ?>
