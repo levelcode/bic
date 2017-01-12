f8203340f55135e6e9ea3c90e01ca7c743b9c581
k4milo <kmilo.vanegas@gmail.com>
2017-01-12 05:35:55 -0500

    formulario markup asistente de botones

--- theme-bic/assets/js/main.js
+++ theme-bic/assets/js/main.js
@@ -2,7 +2,35 @@
 
 	//form asistance
 	var $formCont = $('#suscription_form');
-	
+
 		$formCont.find('.step-1').addClass('active');
+
+		//prevent default
+		$formCont.find('button').click(function(event) {
+			event.preventDefault();
+		});
+
+		//step 1
+		$('.step-1-next').click(function(event) {
+			$formCont.find('.step-1').removeClass('active');
+			$formCont.find('.step-2').addClass('active');
+		});
+
+		//step 2
+		$('.step-2-next').click(function(event) {
+			$formCont.find('.step-2').removeClass('active');
+			$formCont.find('.step-3').addClass('active');
+		});
+
+		$('.step-2-back').click(function(event) {
+			$formCont.find('.step-1').addClass('active');
+			$formCont.find('.step-2').removeClass('active');
+		});
+
+		//step 3
+		$('.step-3-back').click(function(event) {
+			$formCont.find('.step-2').addClass('active');
+			$formCont.find('.step-3').removeClass('active');
+		});
 	
 })(jQuery);
