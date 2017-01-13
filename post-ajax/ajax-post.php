4f8147cb8a3bc2601ee585fd863e85176810316d
Camilo Vanegas <kmilo.vanegas@gmail.com>
2017-01-13 17:26:24 -0500

    funcion inicial para subir imagenes

--- post-ajax/ajax-post.php
+++ post-ajax/ajax-post.php
@@ -31,15 +31,60 @@
    $correo = $_POST['email'];
    $telefono = $_POST['tel'];
    $categoria = $_POST['category'];
+   $fotoDibujo = $_FILES['file-draw'];
+   $fotoArtista = $_FILES['file-photo'];
+   //Upload files
+
 
    //Conditional for duplicate post
 
    if (!get_page_by_title($title, 'OBJECT', 'post') ){
 
+     //add post
+     $post_id = wp_insert_post( array(
+         'post_title'        => $title,
+         'post_status'       => 'publish',
+         'post_author'       => '2'
+     ));
+
+     //Update meta-data__comments
+     update_post_meta($post_id,'Correo',$correo);
+     update_post_meta($post_id,'Celular',$telefono);
+     update_post_meta($post_id,'Documento',$documento);
+     update_post_meta($post_id,'Nombre',$nombreArtista);
+     update_post_meta($post_id,'Responsable',$nombreResponsable);
+     //get permalink
+
+     $link_post = get_permalink($post_id);
+
+     agp_process_woofile($fotoDibujo, $post_id, $nombreArtista);
+     agp_process_woofile($fotoArtista, $post_id, $nombreArtista );
+
    }else{
        $error = "El Nombre de post ya existe";
    }
-   
+ }
+
+
+ //Function for upload files
+ function agp_process_woofile($files, $post_id, $caption){
+     require_once(ABSPATH . "wp-admin" . '/includes/image.php');
+     require_once(ABSPATH . "wp-admin" . '/includes/file.php');
+     require_once(ABSPATH . "wp-admin" . '/includes/media.php');
+
+     $attachment_id = media_handle_upload($files, $post_id);
+
+     $attachment_url = wp_get_attachment_url($attachment_id);
+     add_post_meta($post_id, '_file_paths', $attachment_url);
+
+     $attachment_data = array(
+       'ID' => $attachment_id,
+       'post_excerpt' => $caption
+     );
+
+     wp_update_post($attachment_data);
+
+     return $attachment_id;
  }
 
  // Llamado ajax api wordpress
